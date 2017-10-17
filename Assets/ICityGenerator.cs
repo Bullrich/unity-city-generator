4804f523afe26f0ddd26ad0b5019912e29a26d31
Javier Bullrich <jbullrich@jamcity.com>
2017-10-17 18:38:35 -0300

    Base commit of unity proyect

---
+++ Assets/ICityGenerator.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// By @Bullrich
+namespace CityGenerator
+{
+	public class ICityGenerator : MonoBehaviour {
+	
+	}
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Unity City Generator
+
+Unity Editor Tool of a city generator.
+
+Made by Javier Bullrich, Juan Cruz Araujo and Ivan Ramos
