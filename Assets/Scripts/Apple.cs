772d22cb191d74471f555265d5accde08d5e9cb6
Javyer <javierbullrich@gmail.com>
2017-10-21 20:40:22 -0300

    Completed set of instructions and abstracted the code

---
+++ Assets/Scripts/Apple.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CityGenerator
+{
+    public class Apple
+    {
+        public Vector2 size { get; private set; }
+        public Lot[] appleLots { get; private set; }
+
+        public Apple(Vector2 gridSize, Lot[] _appleLots)
+        {
+            size = gridSize;
+            appleLots = _appleLots;
+            Debug.Log(size);
+            Debug.Log(new Vector3(size.x * BuildCity.buildingFootprint, 0, size.y * BuildCity.buildingFootprint));
+        }
+
+        public GameObject BuildApple()
+        {
+            // generate apple based in the size and store all the apple under a gameobject. Then return such gameobject
+            return null;
+        }
+    }
+}
