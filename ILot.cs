4df32a2571ee9aed1b520aa0da5f2ed72096575e
juancruzaraujo <32915817+juancruzaraujo@users.noreply.github.com>
2017-11-06 21:15:41 -0300

    normalización
    espero no romper nada

---
+++ ILot.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using UnityEngine;
+
+namespace CityGenerator
+{
+    public interface ILot
+    {
+        Vector2 gridPos { get; }
+        Vector3 worldPos { get; set; }
+        GameObject[] buildings { get; }
+        LotType lotType { get; }
+    }
+
+    public class Lot : ILot
+    {
+        public Vector2 gridPos { get; private set; }
+        public Vector3 worldPos { get; set; }
+        public GameObject[] buildings { get; private set; }
+        public Lot neighboor;
+
+        public LotType lotType
+        {
+            get { return LotType.Lot; }
+        }
+
+        public Lot(Vector2 _gridPos, Neighborhood neighborhood)
+        {
+            gridPos = _gridPos;
+            buildings = neighborhood.buildings;
+        }
+    }
+
+    public class Street : ILot
+    {
+        public Vector2 gridPos { get; private set; }
+        public Vector3 worldPos { get; set; }
+
+        public GameObject[] buildings { get; private set; }
+
+        public LotType lotType
+        {
+            get { return LotType.Street; }
+        }
+
+        public enum streetType
+        {
+            xStreet,
+            zStreert,
+            Crossroad
+        }
+
+        public streetType street { get; private set; }
+
+        public Street(GameObject street, Vector2 _gridPos)
+        {
+            buildings = new GameObject[1] {street};
+            gridPos = _gridPos;
+        }
+    }
+
+    public enum LotType
+    {
+        Street,
+        Lot
+    }
+
+    [System.Serializable]
+    public class Neighborhood
+    {
+        public GameObject[] buildings;
+        [Range(0, 100)] public int ChanceToAppear = 3;
+    }
+}
